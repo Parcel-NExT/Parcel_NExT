be3fdff428bdfbaba74a2159be5bd42e600e77f1
Charles Zhang <szinubuntu@gmail.com>
2024-01-26 11:25:20 -0500

    [Unit Test] [Parcel.CoreEngine.Service] Draft basic procedural logic test

--- C#/Parcel.NExT/UnitTests/Parcel.CoreEngine.Service.UnitTests/BasicProceduralLogicTest.cs
+++ C#/Parcel.NExT/UnitTests/Parcel.CoreEngine.Service.UnitTests/BasicProceduralLogicTest.cs
@@ -15,40 +15,47 @@ namespace Parcel.CoreEngine.Service.UnitTests
             ParcelDocument document = CreateGraph();
             document.Execute();
             ParcelNode previewNode = document.MainGraph.MainLayout.Nodes.Single(n => n.Node.Target == "Preview").Node!;
-            Assert.Equal(20, (double)document.NodePayloadLookUps[previewNode].PayloadData.First().Value);
+            Assert.Equal(5 + 12 + 15, (double)document.NodePayloadLookUps[previewNode].PayloadData.First().Value);
         }
 
         #region Routines
         private ParcelDocument CreateGraph()
         {
             // Set graph variable
-            ParcelNode create = new("SetVariable", new Dictionary<string, string>
+            ParcelNode n1 = new("{SetVariable}", new Dictionary<string, string>
             {
                 { "name", "Test Variable" },
                 { "value", "5" },
             });
 
             // Get graph variable
-            ParcelNode readVariable = new("GetVariable", new Dictionary<string, string>
+            ParcelNode n2 = new("{GetVariable}", new Dictionary<string, string>
             {
                 { "name", "Test Variable" },
             });
 
+            // Make number literal
+            ParcelNode n3 = new("Primitives.Number", new Dictionary<string, string>
+            {
+                { "value", ":12" },
+            });
+
             // Perform mathamtical computation
-            ParcelNode add = new("Primitives.Number.Add");
-            add.Inputs.Add(new ParcelNodeInputDefinition("A", "variable"));
-            add.Inputs.Add(new ParcelNodeInputDefinition("B", "15"));
+            ParcelNode n4 = new("Result", "Primitives.Number.Add");
+            n4.Inputs.Add(new ParcelNodeInputDefinition("A", "$Test Variable"));
+            n4.Inputs.Add(new ParcelNodeInputDefinition("B", ":15"));
+            n4.Inputs.Add(new ParcelNodeInputDefinition("C", "@Literal"));
 
             // Hook up preview node
-            ParcelNode preview = new("Preview");
-            preview.Inputs.Add(new ParcelNodeInputDefinition("source", "Result.value"));
+            ParcelNode n5 = new("{}");
+            n5.Inputs.Add(new ParcelNodeInputDefinition("source", "@Result.value"));
 
             // Create document
             ParcelDocument document = new();
             var layout = document.MainGraph.Layouts.First();
 
             // Add nodes
-            ParcelNode[] nodes = [create, readVariable, add, preview];
+            ParcelNode[] nodes = [n1, n2, n3, n4, n5];
             for (int i = 0; i < nodes.Length; i++)
             {
                 ParcelNode node = nodes[i];
