41f3404679179bf254ac4e945a6afc560f51a121
Charles Zhang <szinubuntu@gmail.com>
2024-01-27 14:53:41 -0500

    [MiniParcel] Complete preliminary CLI behaviors

--- C#/Parcel.NExT/FrontEnds/MiniParcel/Program.cs
+++ C#/Parcel.NExT/FrontEnds/MiniParcel/Program.cs
@@ -1,15 +1,94 @@
-﻿using Parcel.CoreEngine.MiniParcel;
+﻿using Parcel.CoreEngine;
+using Parcel.CoreEngine.MiniParcel;
 using Parcel.CoreEngine.Service.CoreExtensions;
+using Parcel.CoreEngine.Versioning;
+using System.Text;
 
 namespace MiniParcel
 {
     internal class Program
     {
+        #region Sub-Commands
+        private static Dictionary<string, Action<string[]>> SubCommands = new ()
+        {
+            { "Convert", Convert }
+        };
+        #endregion
+
+        #region Entry Point
         static void Main(string[] args)
         {
-            string inputFile = args.First();
-            var document = MiniParcelService.Parse(File.ReadAllText(inputFile));
-            document.Execute();
+            // REPL
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"MiniParcel: REPL & CLI for Parcel (Engine Version: {EngineVersion.Version})");
+                REPL();
+            }
+            // Print help
+            else if (args.Length == 1 && args.First() == "--help")
+                PrintHelp();
+            // Run script
+            else if (File.Exists(args.First()))
+                RunScript(Path.GetFullPath(args.First()), args.Skip(1).ToArray());
+            // Run subcommands
+            else if (SubCommands.ContainsKey(args.First()))
+                RunCommand(SubCommands[args.First()], args.Skip(1).ToArray());
+            // Invalid
+            else
+            {
+                Console.WriteLine($"Invalid command line format: {Environment.CommandLine}");
+                PrintHelp();
+            }
         }
+
+        #endregion
+
+        #region Execution Paths
+        private static void REPL()
+        {
+            StringBuilder script = new();
+            while (true)
+            {
+                Console.Write("> ");
+                string? input = Console.ReadLine();
+                if (input == null || input == "exit")
+                    break;
+
+                script.AppendLine(input);
+            }
+            
+            MiniParcelService.Parse(script.ToString()).Execute();
+        }
+        private static void PrintHelp()
+        {
+            Console.WriteLine($"""
+                Arguments:
+                  (No Argument): Enter REPL mode.
+                  --help: Print this help.
+                  <FilePath> [<Arguments>...]: Run document.
+                  <Command> [<Arguments>...]: Run subcommand.
+                Subcommands:
+                  {string.Join("\n  ", SubCommands.Keys)}
+                """);
+        }
+        private static void RunScript(string inputFile, string[] strings)
+        {
+            string script = File.ReadAllText(inputFile);
+            MiniParcelService.Parse(script).Execute();
+        }
+        private static void RunCommand(Action<string[]> action, string[] arguments)
+            => action(arguments);
+        #endregion
+
+        #region Sub-Commands
+        public static void Convert(string[] arguments)
+        {
+            string inputFile = arguments.First();
+            string outputFile = arguments.Last();
+
+            ParcelDocument document = MiniParcelService.Parse(File.ReadAllText(inputFile));
+            document.Save(outputFile);
+        }
+        #endregion
     }
 }
