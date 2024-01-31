00852d2eed78f1258b9a76ad97585ad052697f9f
Charles Zhang <szinubuntu@gmail.com>
2024-01-31 09:03:21 -0500

    [Backend] Create wrapper InteractivePython

---
+++ C#/Parcel.NExT/BackEnds/InteractivePython/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace InteractivePython
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate single instance
+            Parcel.NExT.Python.InteractivePython python = new();
+
+            // Remark-cz: At the moment we use a simple standard input/output model
+            StringBuilder scripts = new();
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (input != null)
+                    scripts.AppendLine(input);
+                else if (!string.IsNullOrEmpty(scripts.ToString()))
+                {
+                    EvaluateScript(python, scripts.ToString().TrimEnd());
+                    scripts.Clear();
+                }
+            }
+        }
+
+        private static void EvaluateScript(Parcel.NExT.Python.InteractivePython python, string script)
+        {
+            object? result = RunScript(python, script);
+            if (result != null)
+                Console.WriteLine(result.ToString());
+        }
+
+        private static object? RunScript(Parcel.NExT.Python.InteractivePython python, string scripts)
+        {
+            object? obj = python.Evaluate(scripts);
+            return obj;
+        }
+    }
+}
---
+++ C#/Parcel.NExT/BackEnds/InteractivePython/README.md
@@ -0,0 +1,3 @@
+# Interactive Python
+
+A standalone wrapper process working around the GIL limit of python - we have to have such processes in order to communicate with multiple standalone pythons. (Likely the same thing applies to C#/Pure)
