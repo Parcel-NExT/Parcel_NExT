69e5c2c52f67ddb2815614812a32433974f64c40
Chaojian Zhang <szinubuntu@gmail.com>
2024-02-09 12:58:56 -0500

    [Tranquility] Update CLI parsing

--- C#/Parcel.NExT/BackEnds/Tranquility/Program.cs
+++ C#/Parcel.NExT/BackEnds/Tranquility/Program.cs
@@ -2,21 +2,25 @@
 
 namespace Tranquility
 {
+    public class TranquilityOptions
+    {
+        public string ServerAddress { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string envVar = "PARCEL_TRANQUILITY_SERVER_ADDRESS";
-            const string defaultAddress = "ws://localhost:9915";
+            if (args.Length == 0 || args.FirstOrDefault() == "--help")
+            {
+                PrintHelp();
+                return;
+            }
 
-            string serverAddress = args.Length > 0
-                ? args[0]
-                : Environment.GetEnvironmentVariable(envVar) != null
-                    ? Environment.GetEnvironmentVariable(envVar)!
-                    : defaultAddress;
+            TranquilityOptions options = ParseOptions(args);
 
-            Logging.Info($"Start {nameof(Tranquility)} at {serverAddress}...");
-            WebSocketServer wssv = new(serverAddress);
+            Logging.Info($"Start {nameof(Tranquility)} at {options.ServerAddress}...");
+            WebSocketServer wssv = new(options.ServerAddress);
 
             wssv.AddWebSocketService<TranquilitySession>("/Tranquility");
             wssv.Start();
@@ -24,6 +28,55 @@ namespace Tranquility
             Console.WriteLine("Tranquility is started. Press any key to quit.");
             Console.ReadKey(true);
             wssv.Stop();
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("""
+                Tranquility is the websocket backend server for Parcel NExT.
+                Typically it should be started by the front-end on an as-needed basis.
+                Command line options:
+                    --help: Print this help message.
+                    --address: The address to listen on. Default is ws://localhost:9915.
+                    --default: Start tranquility using default options.
+                """);
+        }
+
+        private static TranquilityOptions? ParseOptions(string[] args)
+        {
+            const string envVar = "PARCEL_TRANQUILITY_SERVER_ADDRESS";
+
+            // Defaults
+            string serverAddress = Environment.GetEnvironmentVariable(envVar) != null
+                ? Environment.GetEnvironmentVariable(envVar)!
+                : "ws://localhost:9915";
+
+            string? keyword = null;
+            string[] validOptions = ["--default", "--address"];
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("--"))
+                    keyword = arg;
+                else if (keyword == null)
+                {
+                    Console.WriteLine($"Invalid argument format: {arg}");
+                    return null;
+                }
+                else
+                {
+                    switch (keyword)
+                    {
+                        case "--address":
+                            serverAddress = arg;
+                            break;
+                    }
+                }
+            }
+
+            return new TranquilityOptions()
+            {
+                ServerAddress = serverAddress
+            };
         }
     }
 }
