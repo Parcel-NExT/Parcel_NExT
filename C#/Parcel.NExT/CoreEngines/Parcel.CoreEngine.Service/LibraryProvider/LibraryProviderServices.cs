375a4a4fc1cf5ae6c5b48a744c971fa4ba607300
Charles Zhang <szinubuntu@gmail.com>
2024-02-17 17:52:27 -0500

    [Service Provider] Refine node indexing service

--- C#/Parcel.NExT/CoreEngines/Parcel.CoreEngine.Service/LibraryProvider/LibraryProviderServices.cs
+++ C#/Parcel.NExT/CoreEngines/Parcel.CoreEngine.Service/LibraryProvider/LibraryProviderServices.cs
@@ -4,6 +4,20 @@ using System.Reflection;
 
 namespace Parcel.CoreEngine.Service.LibraryProvider
 {
+    public enum EndPointNature
+    {
+        Type,
+        StaticMethod,
+        InstanceMethod,
+        System // Requires special handling from either frontend or backend or runtime
+    }
+    /// <summary>
+    /// Conceptually either Type or Method will be available as per EndPointNature; 
+    /// However, since all methods in C# have a type, when referring to C# endpoints, Type is always available, even for static methods;
+    /// For Python, however, since there is no Type, and since there is no C# MethodInfo, only Identifier will be available.
+    /// </summary>
+    public record TargetEndPoint(EndPointNature Nature, string Identifier, Type? Type, MethodInfo? Method);
+
     /// <summary>
     /// Single-entry provider for all service-inquiry related functions.
     /// Methods exposed here are backbone for all backend services related to service inquiry.
@@ -14,6 +28,19 @@ namespace Parcel.CoreEngine.Service.LibraryProvider
     /// </remarks>
     public class LibraryProviderServices
     {
+        #region Properties
+        private Dictionary<string, TargetEndPoint>? _targetEndPoints;
+        private Dictionary<string, TargetEndPoint> TargetEndPoints
+        {
+            get
+            {
+                if (_targetEndPoints == null)
+                    _targetEndPoints = IndexTargetEndPoints();
+                return _targetEndPoints;
+            }
+        }
+        #endregion
+
         #region Runtime Queries
         public string[] GetAvailableRuntimes()
         {
@@ -61,7 +88,7 @@ namespace Parcel.CoreEngine.Service.LibraryProvider
                 "System",
                 "MathNet",
                 "Roslyn",
-                "PyTorch"
+                "PyTorch" // We need to try our best to provide available Python modules as well
             ];
         }
         public int GetAvailableModulesCount()
@@ -77,6 +104,13 @@ namespace Parcel.CoreEngine.Service.LibraryProvider
         /// </remarks>
         public string[] GetAllTargetPaths()
         {
+            return [.. TargetEndPoints.Keys];
+        }
+        #endregion
+
+        #region Routines
+        private static Dictionary<string, TargetEndPoint> IndexTargetEndPoints()
+        {
             // TODO: At the moment we are excluding system to avoid overhead, in the future we definitely want to expose all native runtime targets as well
             // TODO: We also need to make sure behaviors of this function is consistent with many other module related queries above
             // TODO: Might also want to expose public properties and members
@@ -86,12 +120,18 @@ namespace Parcel.CoreEngine.Service.LibraryProvider
             MethodInfo[] exportedStaticMethods = exportedTypes
                 .SelectMany(t => t.GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy))
                 .ToArray();
-            return [
-                // TODO: Standardize path name and make sure identifiable
-                .. exportedTypes.Select(t => t.Name), 
-                .. exportedStaticMethods.Select(m => m.Name), // TODO: Remark: Notice we are exporting just the names of methods because we consider them "top-level"
-                .. SystemNodes.ReservedNodeTargetNames
-            ];
+
+            Dictionary<string, TargetEndPoint> endpoints = [];
+            // TODO: Standardize path name and make sure identifiable
+            foreach (Type type in exportedTypes)
+                endpoints.Add(type.Name, new TargetEndPoint(EndPointNature.Type, type.Name, type, null));
+            foreach (MethodInfo method in exportedStaticMethods)
+                // TODO: Remark: Notice we are exporting just the names of methods because we consider them "top-level"
+                endpoints.Add(method.Name, new TargetEndPoint(EndPointNature.StaticMethod, method.Name, method.DeclaringType, method));
+            foreach (var name in SystemNodes.ReservedNodeTargetNames)
+                endpoints.Add(name, new TargetEndPoint(EndPointNature.System, name, null, null));
+
+            return endpoints;
         }
         #endregion
     }
