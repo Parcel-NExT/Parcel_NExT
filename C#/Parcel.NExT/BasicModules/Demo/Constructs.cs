a67305f367c71d13c8bc03b81044d2ef8c616352
Charles Zhang <szinubuntu@gmail.com>
2024-02-19 12:15:12 -0500

    [Demo] Update function interface
    - Use semantic Type `Text` for code blocks

--- C#/Parcel.NExT/BasicModules/Demo/Constructs.cs
+++ C#/Parcel.NExT/BasicModules/Demo/Constructs.cs
@@ -1,4 +1,5 @@
-﻿using Parcel.NExT.Interpreter.Scripting;
+﻿using Parcel.CoreEngine.SemanticTypes;
+using Parcel.NExT.Interpreter.Scripting;
 
 namespace Demo
 {
@@ -7,9 +8,9 @@ namespace Demo
         /// <summary>
         /// A simple execute code node: executes code in a local function scope. The code must have a single return string return value that tells which next exec pin to pick.
         /// </summary>
-        public static string Exec(string codeFragment)
+        public static string Exec(Text codeFragment)
         {
-            return ContextFreeRoslyn.EvaluateLocalReturn<string>(codeFragment);
+            return ContextFreeRoslyn.EvaluateLocalReturn<string>(codeFragment.Value);
         }
     }
 }
