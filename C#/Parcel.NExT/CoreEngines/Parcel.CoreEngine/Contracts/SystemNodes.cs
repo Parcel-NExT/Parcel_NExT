af7768049686d8e817e16c5ae43ee1ecde6a02b9
Charles Zhang <szinubuntu@gmail.com>
2024-02-19 14:06:16 -0500

    [Contract] Change naming convention of reserved node targets

--- C#/Parcel.NExT/CoreEngines/Parcel.CoreEngine/Contracts/SystemNodes.cs
+++ C#/Parcel.NExT/CoreEngines/Parcel.CoreEngine/Contracts/SystemNodes.cs
@@ -7,20 +7,20 @@
         /// TODO: Can we make those unique names even shorter?
         /// </summary>
         public static readonly string[] ReservedNodeTargetNames = [
-            "Parcel-Construct:Graph", // Denotes a graph reference
-            "Parcel-Construct:Subgraph", // Denotes a subgraph region
+            "Parcel.Graph:Construct", // Denotes a graph reference
+            "Parcel.Subgraph:Construct", // Denotes a subgraph region
 
-            "Parcel-Construct:Custom", // Denotes a customly specified target path
+            "Parcel.Custom:Construct", // Denotes a customly specified target path
 
-            "Parcel-Construct:Set", // Sets value for an object property (e.g. struct or class member)
-            "Parcel-Construct:Get", // Get value of an object property (e.g. struct or class member)
+            "Parcel.Set:Construct", // Sets value for an object property (e.g. struct or class member)
+            "Parcel.Get:Construct", // Get value of an object property (e.g. struct or class member)
 
-            "Parcel-Construct:FunctionStart", // Denotes functional function start
-            "Parcel-Construct:FunctionReturn",  // Denotes functional function return/end
+            "Parcel.FunctionStart:Construct", // Denotes functional function start
+            "Parcel.FunctionReturn:Construct",  // Denotes functional function return/end
 
-            "Parcel-Core:Print",  // Print messages to standard output/console (affects both backend and front-end)
+            "Parcel.Print:Core",  // Print messages to standard output/console (affects both backend and front-end)
 
-            "Parcel-Frontend:Preview", // Denotes a preview node
+            "Parcel.Preview:Frontend", // Denotes a preview node
         ];
     }
 }
