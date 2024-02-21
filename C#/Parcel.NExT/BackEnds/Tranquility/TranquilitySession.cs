45506c01a2cab76781343808d3be5a8317138735
Charles Zhang <szinubuntu@gmail.com>
2024-02-21 08:50:29 -0500

    [Protocol] Simplify MULTIPART message syntax

--- C#/Parcel.NExT/BackEnds/Tranquility/TranquilitySession.cs
+++ C#/Parcel.NExT/BackEnds/Tranquility/TranquilitySession.cs
@@ -80,10 +80,10 @@ namespace Tranquility
                 int segments = (int)Math.Ceiling(message.Length / (double)sizeLimit);
                 for (int i = 0; i < segments; i++)
                 {
-                    string fragmentHeader = $"MULTIPART: {i+1}/{segments} ({message.Length})";
+                    string fragmentHeader = $"MULTIPART:{i+1} {segments} {message.Length}"; // We make this format simple so for Gospel it's less parsing.
                     string fragment = message.Substring(i * sizeLimit, Math.Min(message.Length - i * sizeLimit, sizeLimit));
                     Send($"{fragmentHeader}\n{fragment}");
-                    Thread.Sleep(5); // Remark-cz: (Hack) Give front-end some processing time before buffer fills up. We have tested that on localhost, both 100ms, 10ms, 5ms and 1ms seems to work - though it largely depends on how fast frontend (Gospel) can digest it. If this time is to short, Godot might either simply run out of buffer memory and output error, or just drop packets silently.
+                    Thread.Sleep(5); // Remark-cz: (Hack) Give front-end some processing time before buffer fills up. We have tested that on localhost, both 100ms, 10ms, 5ms and 1ms seems to work - though it largely depends on how fast frontend (Gospel) can digest it. If this time is to short, Godot might either simply run out of buffer memory and output error, or just drop packets silently. 1ms works on a fast PC, while 5ms is minimal requirement for a slow laptop.
                 }
             }
         }
