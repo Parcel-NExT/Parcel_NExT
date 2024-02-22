40d684be1d6a93f1b6ee85b150df53522517f649
Chaojian Zhang <szinubuntu@gmail.com>
2024-02-22 15:53:03 -0500

    [Tranquility] Refactor and update unit test
    - Fix messaging format

--- C#/Parcel.NExT/UnitTests/Tranquility.UnitTests/BasicConnectionTest.cs
+++ C#/Parcel.NExT/UnitTests/Tranquility.UnitTests/BasicConnectionTest.cs
@@ -1,6 +1,4 @@
 using Parcel.CoreEngine.Service.LibraryProvider;
-using WebSocketSharp;
-using WebSocketSharp.Server;
 
 namespace Tranquility.UnitTests
 {
@@ -9,27 +7,9 @@ namespace Tranquility.UnitTests
         [Fact]
         public void BasicCommunicationTest()
         {
-            int port = Program.FindNextFreeTcpPort();
-            string address = $"ws://localhost:{port}";
-            string endpoint = "/Tranquility";
-
-            WebSocketServer server = new(address);
-            server.AddWebSocketService<TranquilitySession>(endpoint);
-            server.Start();
-
-            int count = 0;
-            var client = new WebSocket($"{address}{endpoint}");
-            client.OnMessage += (sender, e) =>
-                count = e.Data.Split('\n').Length;
-            var task = Task.Run(() =>
-            {
-                client.Connect();
-                client.Send(nameof(LibraryProviderServices.GetAvailableRuntimes));
-            }).Wait(1000);
-
-            Thread.Sleep(2000);
-            client.Close();
-            server.Stop();
+            string request = $"{nameof(LibraryProviderServices)}.{nameof(LibraryProviderServices.GetAvailableRuntimes)}";
+            string replyMessage = TranquilityUnitTestHelper.RunSingleRequest(request);
+            int count = replyMessage.Split('\n').Length;
 
             Assert.Equal(new LibraryProviderServices().GetAvailableRuntimes().Length, count);
         }
---
+++ C#/Parcel.NExT/UnitTests/Tranquility.UnitTests/TranquilityUnitTestHelper.cs
@@ -0,0 +1,44 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Tranquility.UnitTests
+{
+    internal static class TranquilityUnitTestHelper
+    {
+        #region Requests
+        public static string? RunSingleRequest(string message)
+        {
+            int port = Program.FindNextFreeTcpPort();
+            string address = $"ws://localhost:{port}";
+            string endpoint = "/Tranquility";
+
+            WebSocketServer server = new(address);
+            server.AddWebSocketService<TranquilitySession>(endpoint);
+            server.Start();
+
+            string? replyMessage = null;
+            var client = new WebSocket($"{address}{endpoint}");
+            client.OnMessage += (sender, e) =>
+                replyMessage = e.Data;
+            bool task = Task.Run(() =>
+            {
+                client.Connect();
+                client.Send(message);
+            }).Wait(1000);
+
+            Thread.Sleep(2000);
+            client.Close();
+            server.Stop();
+
+            return replyMessage;
+        }
+        #endregion
+
+        #region MyRegion
+        public static object DefaultDeserialization(string message)
+        {
+            return Parcel.CoreEngine.Messaging.StandardDeserialization.Deserialize(message);
+        }
+        #endregion
+    }
+}
