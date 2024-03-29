2bfe849f51a2f3435e3f3629dc60fe8ce80b5d48
Charles Zhang <szinubuntu@gmail.com>
2024-03-29 17:25:53 -0400

    Enhance implementation of basic types

--- C#/Parcel.NExT/BasicModules/StandardLibrary/DependencyFree/BasicMath.cs
+++ C#/Parcel.NExT/BasicModules/StandardLibrary/DependencyFree/BasicMath.cs
@@ -4,5 +4,13 @@
     {
         public static double Add(double a, double b)
             => a + b;
+        public static double Subtract(double a, double b)
+            => a - b;
+        public static double Multiply(double a, double b)
+            => a * b;
+        public static double Divide(double a, double b)
+            => a / b;
+        public static double Power(double @base, double exponent)
+            => Math.Pow(@base, exponent);
     }
 }
