1884db647ad3ecb80bff0d5616c6790dee1d2a8d
Charles Zhang <szinubuntu@gmail.com>
2024-06-09 15:15:44 -0400

    Migrate utility class from Pure 2
    - Consolidate dependency version

---
+++ C#/Parcel.NExT/CoreEngines/Parcel.CoreEngine/Helpers/Measure.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Parcel.CoreEngine.Helpers
+{
+    public class Measure : IDisposable
+    {
+        private readonly Stopwatch Timer;
+        public Measure()
+        {
+            Timer = new Stopwatch();
+            Timer.Start();
+        }
+        public void Dispose()
+        {
+            Timer.Stop();
+            TimeSpan timeTaken = Timer.Elapsed;
+            Console.WriteLine("Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
+        }
+    }
+}
