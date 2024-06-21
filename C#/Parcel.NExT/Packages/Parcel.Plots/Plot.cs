bd90922f194b17b457c82fdfdf9a199daf15fa6f
Charles Zhang <szinubuntu@gmail.com>
2024-06-21 17:28:49 -0400

    [Package] Draft Plot package

---
+++ C#/Parcel.NExT/Packages/Parcel.Plots/Plot.cs
@@ -0,0 +1,69 @@
+﻿namespace Parcel.Graphing
+{
+    public static class Plot
+    {
+        public static string Scatter(double[] x, double[] y, int imageWidth, int imageHeight, string title, string xAxis, string yAxis, string legend)
+        {
+            ScottPlot.Plot plot = new();
+            var s = plot.Add.Scatter(x, y);
+            if (!string.IsNullOrEmpty(legend))
+            {
+                plot.Legend.IsVisible = true;
+                s.LegendText = legend;
+            }
+
+            if (!string.IsNullOrEmpty(title))
+                plot.Title(title);
+            if (!string.IsNullOrEmpty(xAxis))
+                plot.Axes.Left.Label.Text = xAxis;
+            if (!string.IsNullOrEmpty(yAxis))
+                plot.Axes.Bottom.Label.Text = yAxis;
+
+            string path = GetTempImagePath();
+            plot.SavePng(path, imageWidth == 0 ? 400 : imageWidth, imageHeight == 0 ? 300 : imageHeight);
+            return $"Image://{path}";
+        }
+        public static string Line(double[] x, double[] y, int imageWidth, int imageHeight, string title, string xAxis, string yAxis, string legend)
+        {
+            ScottPlot.Plot plot = new();
+            if (!string.IsNullOrEmpty(legend))
+            {
+                var s = plot.Add.Scatter(x, y);
+                s.LegendText = legend;
+            }
+
+            if (!string.IsNullOrEmpty(title))
+                plot.Title(title);
+            if (!string.IsNullOrEmpty(xAxis))
+                plot.Axes.Left.Label.Text = xAxis;
+            if (!string.IsNullOrEmpty(yAxis))
+                plot.Axes.Bottom.Label.Text = yAxis;
+
+            string path = GetTempImagePath();
+            plot.SavePng(path, imageWidth == 0 ? 400 : imageWidth, imageHeight == 0 ? 300 : imageHeight);
+            return $"Image://{path}";
+        }
+        public static string Histogram(double[] v, int imageWidth, int imageHeight, string title, string xAxis, string yAxis, int histogramBars)
+        {
+            ScottPlot.Plot plot = new();
+            
+            ScottPlot.Statistics.Histogram hist = new(min: v.Min(), max: v.Max(), binCount: histogramBars);
+            hist.AddRange(v);
+            plot.Add.Bars(values: hist.Counts, positions: hist.Bins);
+
+            if (!string.IsNullOrEmpty(title))
+                plot.Title(title);
+            if (!string.IsNullOrEmpty(xAxis))
+                plot.Axes.Left.Label.Text = xAxis;
+            if (!string.IsNullOrEmpty(yAxis))
+                plot.Axes.Bottom.Label.Text = yAxis;
+
+            string path = GetTempImagePath();
+            plot.SavePng(path, imageWidth == 0 ? 400 : imageWidth, imageHeight == 0 ? 300 : imageHeight);
+            return $"Image://{path}";
+        }
+
+        private static string GetTempImagePath()
+            => Path.GetTempPath() + Guid.NewGuid().ToString() + ".png";
+    }
+}
---
+++ C#/Parcel.NExT/Packages/Parcel.Plots/README.md
@@ -0,0 +1,3 @@
+# Parcel.Plots
+
+Unfortunately, image rendering is platform dependent,and front-end dependent, so without creating all that bitmap, image, drawing infrastructure ourselves, this particular library at the moment handles everything as plain files.
