700bd72d0ac020d55a419acb9f863663c86f3962
Charles Zhang <szinubuntu@gmail.com>
2024-06-24 10:10:20 -0400

    [Package] Update Vector methods

--- C#/Parcel.NExT/Packages/Parcel.Vector/Vector.cs
+++ C#/Parcel.NExT/Packages/Parcel.Vector/Vector.cs
@@ -2,10 +2,21 @@
 {
     public static class VectorHelper
     {
+        #region Generation
         public static double[] Range(int count = 100, double start = 0, double increment = 1)
+            => Enumerable.Range(0, count).Select(x => start + x * increment).ToArray();
+        #endregion
+
+        #region Collection
+        public static double[] Shift(this double[] vector, int offset, bool fillZero)
         {
-            return Enumerable.Range(0, count).Select(x => start + x * increment).ToArray();
+            // TODO: Boundary check
+            if (offset > 0) // Shift right
+                return vector.Skip(offset).Concat(fillZero ? new double[offset] : vector.Take(offset)).ToArray();
+            else // Shift left
+                return vector.Skip(vector.Length - 1).Concat(fillZero ? new double[offset] : vector.Take(vector.Length - 1)).ToArray();
         }
+        #endregion
 
         #region Math
         public static double[] Subtract(this double[] vector, double other)
@@ -50,6 +61,47 @@
             => vector.StandardDeviation();
         public static double Variance(this double[] vector)
             => vector.Variance();
+        #endregion
+
+        #region Operations
+        public enum AccumulateApplyFunction
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+        /// <summary>
+        /// Applies certain operation starting from base value on top of entire vector and return the resulting vector and final value
+        /// </summary>
+        public static double AccumulateApply(double baseValue, double[] vector, out double[] path, AccumulateApplyFunction function)
+        {
+            switch (function)
+            {
+                case AccumulateApplyFunction.Add:
+                    return AccumulateApply(baseValue, vector, out path, (previous, currentElement) => previous + currentElement);
+                case AccumulateApplyFunction.Subtract:
+                    return AccumulateApply(baseValue, vector, out path, (previous, currentElement) => previous - currentElement);
+                case AccumulateApplyFunction.Multiply:
+                    return AccumulateApply(baseValue, vector, out path, (previous, currentElement) => previous * currentElement);
+                case AccumulateApplyFunction.Divide:
+                    return AccumulateApply(baseValue, vector, out path, (previous, currentElement) => previous / currentElement);
+                default:
+                    throw new ArgumentException($"Invalid function: {function}");
+            }
+        }
+        /// <param name="function">Given previous computed result and current element value, compute the next value</param>
+        public static double AccumulateApply(double baseValue, double[] vector, out double[] path, Func<double, double, double> function)
+        {
+            double finalValue = baseValue;
+            path = new double[vector.Length];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                double newValue = function(finalValue, vector[i]);
+                path[i] = newValue;
+            }
+            return finalValue;
+        }
         #endregion
     }
 }
