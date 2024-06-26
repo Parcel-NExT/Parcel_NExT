2172798016496bd60740b5ef4254863dfdbcc9f5
Charles Zhang <szinubuntu@gmail.com>
2024-06-26 13:30:55 -0400

    [Package] Early draft of MathConstants

---
+++ C#/Parcel.NExT/Packages/Parcel.MathConstants/BasicConstants/MathConstants.cs
@@ -0,0 +1,79 @@
+﻿namespace Parcel.Fact.BasicConstants
+{
+    public static class MathematicalConstants
+    {
+        public const double E = 2.71828182845904;
+        public const double Log2E = 1.44269504088896;
+        public const double Log10E = 0.43429448190325;
+        public const double Ln2 = 0.69314718055995;
+        public const double Ln10 = 2.30258509299404;
+        public const double LnPi = 1.14472988584940;
+        public const double Ln2PiOver2 = 0.91893853320467;
+        public const double InvE = 0.36787944117144;
+        public const double SqrtE = 1.64872127070012;
+        public const double Sqrt2 = 1.41421356237309;
+        public const double Sqrt3 = 1.73205080756887;
+        public const double Sqrt1Over2 = 0.70710678118655;
+        public const double HalfSqrt3 = 0.86602540378444;
+        public const double Pi = 3.14159265358979;
+        public const double Pi2 = 6.28318530717958;
+        public const double PiOver2 = 1.57079632679489;
+        public const double Pi3Over2 = 4.71238898038468;
+        public const double PiOver4 = 0.78539816339745;
+        public const double SqrtPi = 1.77245385090551;
+        public const double Sqrt2Pi = 2.50662827463100;
+        public const double Sqrt2PiE = 4.13273135412249;
+        public const double LogSqrt2Pi = 0.91893853320467;
+        public const double LogSqrt2PiE = 1.41893853320467;
+        public const double LogTwoSqrtEOverPi = 0.62078223763525;
+        public const double InvPi = 0.31830988618379;
+        public const double TwoInvPi = 0.63661977236758;
+        public const double InvSqrtPi = 0.56418958354776;
+        public const double InvSqrt2Pi = 0.39894228040143;
+        public const double TwoInvSqrtPi = 1.12837916709551;
+        public const double TwoSqrtEOverPi = 1.86038273420526;
+        public const double Catalan = 0.91596559417722;
+        public const double EulerMascheroni = 0.57721566490153;
+        public const double GoldenRatio = 1.61803398874989;
+        public const double Glaisher = 1.28242712910062;
+        public const double Khinchin = 2.68545200106530;
+    }
+
+    public static class UniversalConstants
+    {
+        public const double SpeedOfLight = 2.99792458e+08;
+        public const double MagneticPermeability = 1.25663706143591e-06;
+        public const double ElectricPermittivity = 8.8541878171937e-12;
+        public const double CharacteristicImpedanceVacuum = 3.7673031346177e+02;
+        public const double GravitationalConstant = 6.67429e-11;
+        public const double PlancksConstant = 6.62606896e-34;
+        public const double DiracsConstant = 1.054571629e-34;
+        public const double PlancksMass = 2.17644e-08;
+        public const double PlancksTemperature = 1.416786e+32;
+        public const double PlancksLength = 1.616253e-35;
+        public const double PlancksTime = 5.39124e-44;
+    }
+
+    public static class ElectromagneticConstants
+    {
+        public const double ElementaryCharge = 1.602176487e-19;
+        public const double MagneticFluxQuantum = 2.067833668e-15;
+        public const double ConductanceQuantum = 7.7480917005e-05;
+        public const double JosephsonConstant = 4.83597891e+14;
+        public const double VonKlitzingConstant = 2.5812807557e+04;
+        public const double BohrMagneton = 9.27400915e-24;
+        public const double NuclearMagneton = 5.05078324e-27;
+    }
+
+    public static class AtomicAndNuclearConstants
+    {
+        public const double FineStructureConstant = 7.2973525376e-03;
+        public const double RydbergConstant = 1.0973731569e+07;
+        public const double BohrRadius = 5.2917720859e-11;
+        public const double HartreeEnergy = 4.35974394e-18;
+        public const double QuantumOfCirculation = 3.6369475199e-04;
+        public const double FermiCouplingConstant = 1.16637e-05;
+        public const double WeakMixingAngle = 2.2256e-01;
+        public const double Avogadro = 6.0221412927e+23;
+    }
+}
