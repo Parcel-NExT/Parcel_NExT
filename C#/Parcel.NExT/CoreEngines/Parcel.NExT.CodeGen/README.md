e92dc89a351d0cbac92c2c72fbe91cf6dad1bb93
Charles Zhang <szinubuntu@gmail.com>
2024-07-04 14:56:21 -0400

    Allow initializing DataGrid directly from csv (zero dependency)

--- C#/Parcel.NExT/CoreEngines/Parcel.NExT.CodeGen/README.md
+++ C#/Parcel.NExT/CoreEngines/Parcel.NExT.CodeGen/README.md
@@ -7,3 +7,5 @@ There are N types of code gen:
 1. Referential: With or without intermediary, calling of library nodes are handled through DLL linking.
 2. Raw: Only subsets of standard libraries are supported - produces code with zero dependency.
 3. Self-contained: Produces single executable, does not support Pure/snippet codes.
+
+Notice CodeGeneration is made available at scale only in Parcel NExT due to the raw C#-based nature of POS.
---
+++ C#/Parcel.NExT/Packages/Parcel.DataGrid/DataGridBuilder.cs
@@ -0,0 +1,76 @@
+﻿namespace Parcel.Types
+{
+    /// <summary>
+    /// Expose strongly named functions
+    /// </summary>
+    public static class DataGridBuilder
+    {
+        public static DataGrid InitializeDataGridFromCsvText(string tableName, string csvText)
+            => new(tableName, MinimalCSV.ParseCSV(csvText, out string[]? headers, true), headers);
+    }
+
+    /// <summary>
+    /// A private local minimal CSV parser for embedding in packages that wishes to have NO external dependencies
+    /// </summary>
+    internal static class MinimalCSV
+    {
+        #region Interface
+        public static IEnumerable<string[]> ParseCSV(this string text, out string[]? headers, bool containsHeader = true)
+        {
+            string[] lines = text.SplitLines(true);
+            if (containsHeader)
+            {
+                headers = SplitCSVLine(lines.First()).ToArray();
+                return lines.Skip(1).Select(line => SplitCSVLine(line).ToArray());
+            }
+            else
+            {
+                headers = null;
+                return lines.Select(line => SplitCSVLine(line).ToArray());
+            }
+        }
+        #endregion
+
+        #region Helpers
+        private static readonly char[] NewLineSeparators = ['\r', '\n'];
+        private static string[] SplitLines(this string text, bool removeEmpty) => text.Split(NewLineSeparators, removeEmpty ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None);
+        private static IEnumerable<string> SplitCSVLine(this string line, char delimiter = ' ')
+        {
+            bool inQuotes = false;
+
+            return line.Split(c =>
+            {
+                if (c == '\"')
+                    inQuotes = !inQuotes;
+
+                return !inQuotes && c == delimiter;
+            })
+                .Select(arg => arg.Trim().TrimMatchingQuotes('\"'))
+                .Where(arg => !string.IsNullOrEmpty(arg));
+        }
+        private static string TrimMatchingQuotes(this string input, char quote)
+        {
+            if ((input.Length >= 2) &&
+                (input[0] == quote) && (input[^1] == quote))
+                return input[1..^1];
+
+            return input;
+        }
+        private static IEnumerable<string> Split(this string str, Func<char, bool> controller)
+        {
+            int nextPiece = 0;
+
+            for (int c = 0; c < str.Length; c++)
+            {
+                if (controller(str[c]))
+                {
+                    yield return str[nextPiece..c];
+                    nextPiece = c + 1;
+                }
+            }
+
+            yield return str[nextPiece..];
+        }
+        #endregion
+    }
+}
