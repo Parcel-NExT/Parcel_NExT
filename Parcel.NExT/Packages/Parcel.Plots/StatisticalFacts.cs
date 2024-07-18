50ee510cfd76fae75e2fa5d0f2ba0045e50fb26e
Charles Zhang <szinubuntu@gmail.com>
2024-07-18 11:19:26 -0400

    [Statistical Facts] Add Random Walk plot

--- Parcel.NExT/Packages/Parcel.Plots/StatisticalFacts.cs
+++ Parcel.NExT/Packages/Parcel.Plots/StatisticalFacts.cs
@@ -7,11 +7,16 @@ namespace Parcel.Graphing
     /// </summary>
     public static class StatisticalFacts
     {
+        #region Configurations
+        private const int PresetImageWidth = 600;
+        private const int PresetImageHeight = 400;
+        #endregion
+
         #region Distributions
         /// <summary>
         /// Plots normal distribution in histogram
         /// </summary>
-        public static Image NormalDistribution(out double[] values, int count = 1000, double mean = 0, double standardDeviation = 1)
+        public static Image PlotNormalDistribution(out double[] values, int count = 1000, double mean = 0, double standardDeviation = 1)
         {
             ScottPlot.Plot plot = new();
             values = ScottPlot.Generate.RandomNormal(count, mean, standardDeviation);
@@ -25,13 +30,13 @@ namespace Parcel.Graphing
             plot.Title("Demo Normal Distribution");
 
             string path = Plot.GetTempImagePath();
-            plot.SavePng(path, 400, 300);
+            plot.SavePng(path, PresetImageWidth, PresetImageHeight);
             return new Image(path);
         }
         /// <summary>
         /// Plots uniform distribution in histogram
         /// </summary>
-        public static Image UniformDistribution(out double[] values, int count = 1000, double min = 0, double max = 1)
+        public static Image PlotUniformDistribution(out double[] values, int count = 1000, double min = 0, double max = 1)
         {
             ScottPlot.Plot plot = new();
             values = ScottPlot.Generate.RandomSample(count, min, max);
@@ -45,15 +50,50 @@ namespace Parcel.Graphing
             plot.Title("Demo Uniform Distribution");
 
             string path = Plot.GetTempImagePath();
-            plot.SavePng(path, 400, 300);
+            plot.SavePng(path, PresetImageWidth, PresetImageHeight);
             return new Image(path);
         }
         /// <summary>
         /// Plots random walk as scatter plot
         /// </summary>
-        public static Image RandomWalk(out double[] values, int count = 1000, double min = 0, double max = 1)
+        public static Image PlotRandomWalk(out double[] values, int count = 1000, double multiply = 1, double offset = 0)
         {
-            throw new NotImplementedException();
+            ScottPlot.Plot plot = new();
+            values = ScottPlot.Generate.RandomWalk(count, multiply, offset);
+            plot.Add.Scatter(values.Select((v, i) => i).ToArray(), values);
+
+            string path = Plot.GetTempImagePath();
+            plot.SavePng(path, PresetImageWidth, PresetImageHeight);
+            return new Image(path);
+        }
+        #endregion
+
+        #region Mathematical Functions
+        /// <summary>
+        /// Plots sin wave between [-1, 1] as scatter plot
+        /// </summary>
+        public static Image PlotSinusoidalWave(out double[] values, int count = 100, double multiply = 1, double offset = 0)
+        {
+            ScottPlot.Plot plot = new();
+            values = ScottPlot.Generate.Sin(count, multiply, offset);
+            plot.Add.Scatter(values.Select((v, i) => i).ToArray(), values);
+
+            string path = Plot.GetTempImagePath();
+            plot.SavePng(path, PresetImageWidth, PresetImageHeight);
+            return new Image(path);
+        }
+        /// <summary>
+        /// Plots cos wave between [-1, 1] as scatter plot
+        /// </summary>
+        public static Image PlotCosineWave(out double[] values, int count = 100, double multiply = 1, double offset = 0)
+        {
+            ScottPlot.Plot plot = new();
+            values = ScottPlot.Generate.Cos(count, multiply, offset);
+            plot.Add.Scatter(values.Select((v, i) => i).ToArray(), values);
+
+            string path = Plot.GetTempImagePath();
+            plot.SavePng(path, PresetImageWidth, PresetImageHeight);
+            return new Image(path);
         }
         #endregion
     }
