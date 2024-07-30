935fae441b24103c7381a0d51788a62f403a2d30
Charles Zhang <szinubuntu@gmail.com>
2024-07-30 11:11:17 -0400

    Implement password node and text node

---
+++ Neo/Parcel.Neo.Base/Framework/ViewModels/Primitives/PasswordNode.cs
@@ -0,0 +1,14 @@
+﻿using Parcel.Neo.Base.Framework.ViewModels.BaseNodes;
+
+namespace Parcel.Neo.Base.Framework.ViewModels.Primitives
+{
+    public class PasswordNode : PrimitiveNode
+    {
+        public PasswordNode()
+        {
+            Title = NodeTypeName = "Password";
+        }
+
+        public override OutputConnector MainOutput => ValueOutput as OutputConnector;
+    }
+}
---
+++ Neo/Parcel.Neo.Base/Framework/ViewModels/Primitives/TextNode.cs
@@ -0,0 +1,14 @@
+﻿using Parcel.Neo.Base.Framework.ViewModels.BaseNodes;
+
+namespace Parcel.Neo.Base.Framework.ViewModels.Primitives
+{
+    public class TextNode : PrimitiveNode
+    {
+        public TextNode()
+        {
+            Title = NodeTypeName = "Text";
+        }
+
+        public override OutputConnector MainOutput => ValueOutput as OutputConnector;
+    }
+}
