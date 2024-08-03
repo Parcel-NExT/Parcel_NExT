01e70c9f2011e1e42e95070ce56f7d0c84ac9bda
Charles Zhang <szinubuntu@gmail.com>
2024-08-03 15:20:36 -0400

    Fix issue with comment node text wrapping
    - Also fix data binding issue

---
+++ Neo/Parcel.Neo/Converters/SizeToVector2DConverter.cs
@@ -0,0 +1,30 @@
+﻿using Parcel.Neo.Base.DataTypes;
+using Parcel.Types;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Parcel.Neo.Converters
+{
+    public class SizeToVector2DConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Vector2D vector2D)
+                return new Size(vector2D.X, vector2D.Y);
+            else if (value == null)
+                return new Size();
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Size size)
+                return new Vector2D(size.Width, size.Height);
+
+            throw new ArgumentException();
+        }
+    }
+}
