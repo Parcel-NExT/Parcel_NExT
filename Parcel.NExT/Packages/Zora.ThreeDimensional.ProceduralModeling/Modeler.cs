8c288c078e998fca2dd7c1d6ce152d8b0365d4c3
Charles Zhang <szinubuntu@gmail.com>
2024-08-03 10:41:41 -0400

    Package project setup

---
+++ Parcel.NExT/Packages/Zora.ThreeDimensional.ProceduralModeling/Modeler.cs
@@ -0,0 +1,14 @@
+﻿namespace Parcel.DomainSpecific.CGI
+{
+    public class Modeler
+    {
+        #region Helper
+        private static void ValidateDependencies()
+        {
+            // Check python availability and version
+
+            // Check bpy availability
+        }
+        #endregion
+    }
+}
---
+++ Parcel.NExT/Packages/Zora.ThreeDimensional.ProceduralModeling/README.md
@@ -0,0 +1,7 @@
+# Zora.ThreeDimensional.ProceduralModeling
+
+Provides preliminary modeling and rendering.
+
+Requires: Python (3.10+), Blender (bpy)
+
+(Procedural Preview result: Box, Bevel)
