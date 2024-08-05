12e613792cc43f13703814a73954a585778c0628
Charles Zhang <szinubuntu@gmail.com>
2024-08-05 16:17:29 -0400

    Refactor code generation

--- Parcel.NExT/Packages/Parcel.DashboardApp/DashboardAppCreator.cs
+++ Parcel.NExT/Packages/Parcel.DashboardApp/DashboardAppCreator.cs
@@ -1,85 +1,109 @@
 ﻿using Parcel.CoreEngine.Helpers;
 using Parcel.NExT.Python.Helpers;
 using Parcel.Standard.System;
+using System.IO;
 
 namespace Parcel.Framework.Dashboard
 {
+    public sealed class DashboardSettings
+    {
+        public string Title { get; set; } = "My Dashboard";
+    }
+
     public static class DashboardAppCreator
     {
         private const string StreamlitProgramName = "streamlit";
 
         #region Service Start
-        public static string ConfigureService()
+        public static DashboardSettings ConfigureDashboard(string title)
+        {
+            return new DashboardSettings 
+            {
+                Title = title 
+            };
+        }
+        public static string ConfigureService(DashboardSettings? settings)
         {
             ValidateDependencies();
+            settings ??= new();
 
+            // Generate bootstrap script
             string workingDirectory = FileSystem.GetTempFolderPath();
-            string script = GenerateScript(workingDirectory);
+            string script = GenerateScript(settings);
+            string scriptFile = Path.Combine(workingDirectory, "Script.py");
+            File.WriteAllText(scriptFile, script);
 
+            // Run background service
             string? streamlit = EnvironmentVariableHelper.FindProgram(StreamlitProgramName);
             if (streamlit != null)
-                ProcessHelper.StartProcessInBackground(true, streamlit, workingDirectory, "run", Path.GetFileName(script));
+                ProcessHelper.StartProcessInBackground(true, streamlit, workingDirectory, "run", Path.GetFileName(scriptFile));
 
-            return string.Empty; // Should return address
+            // Should return address
+            return string.Empty;
         }
         #endregion
 
         #region Routines
-        private static string GenerateScript(string workingDirectory)
+        private static string GenerateScript(DashboardSettings settings)
         {
-            string path = Path.Combine(workingDirectory, "Script.py");
-            File.WriteAllText(path, """"
+            return $$""""
                 import streamlit as st
                 import pandas as pd
                 import numpy as np
 
-                st.title('Uber pickups in NYC')
+                # Routines
+                def setup():
+                    # Initial setup
+                    st.title("{{settings.Title}}")
 
-                DATE_COLUMN = 'date/time'
-                DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-                            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+                    # Remove annoying Deploy button
+                    st.markdown("""
+                        <style>
+                            .reportview-container {
+                                margin-top: -2em;
+                            }
+                            #MainMenu {visibility: hidden;}
+                            .stDeployButton {display:none;}
+                            footer {visibility: hidden;}
+                            #stDecoration {display:none;}
+                        </style>
+                    """, unsafe_allow_html=True)
 
-                @st.cache_data
-                def load_data(nrows):
-                    data = pd.read_csv(DATA_URL, nrows=nrows)
-                    lowercase = lambda x: str(x).lower()
-                    data.rename(lowercase, axis='columns', inplace=True)
-                    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-                    return data
+                def show():
+                    DATE_COLUMN = 'date/time'
+                    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-                data_load_state = st.text('Loading data...')
-                data = load_data(10000)
-                data_load_state.text("Done! (using st.cache_data)")
+                    @st.cache_data
+                    def load_data(nrows):
+                        data = pd.read_csv(DATA_URL, nrows=nrows)
+                        lowercase = lambda x: str(x).lower()
+                        data.rename(lowercase, axis='columns', inplace=True)
+                        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+                        return data
 
-                if st.checkbox('Show raw data'):
-                    st.subheader('Raw data')
-                    st.write(data)
+                    data_load_state = st.text('Loading data...')
+                    data = load_data(10000)
+                    data_load_state.text("Done! (using st.cache_data)")
 
-                st.subheader('Number of pickups by hour')
-                hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-                st.bar_chart(hist_values)
+                    if st.checkbox('Show raw data'):
+                        st.subheader('Raw data')
+                        st.write(data)
 
-                # Some number in the range 0-23
-                hour_to_filter = st.slider('hour', 0, 23, 17)
-                filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+                    st.subheader('Number of pickups by hour')
+                    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+                    st.bar_chart(hist_values)
 
-                st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-                st.map(filtered_data)
+                    # Some number in the range 0-23
+                    hour_to_filter = st.slider('hour', 0, 23, 17)
+                    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-                # Remove annoying Deploy button
-                st.markdown("""
-                    <style>
-                        .reportview-container {
-                            margin-top: -2em;
-                        }
-                        #MainMenu {visibility: hidden;}
-                        .stDeployButton {display:none;}
-                        footer {visibility: hidden;}
-                        #stDecoration {display:none;}
-                    </style>
-                """, unsafe_allow_html=True)
-                """");
-            return path;
+                    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+                    st.map(filtered_data)
+
+                setup()
+                show()
+                """";
         }
         private static void ValidateDependencies()
         {
@@ -97,10 +121,15 @@ namespace Parcel.Framework.Dashboard
             string? pip = EnvironmentVariableHelper.FindProgram(PythonRuntimeHelper.PipExecutableName)
                 ?? throw new FileNotFoundException($"Cannot find pip on current computer.");
 
-            // Check bpy availability
+            // Check streamlit availability
             string? streamlit = PythonRuntimeHelper.GetModuleVersion("streamlit");
             if (!streamlit.StartsWith("1.37"))
                 throw new InvalidProgramException("Expect streamlit 1.37+.");
+
+            // Check pythonet availability
+            string? pythonnet = PythonRuntimeHelper.GetModuleVersion("pythonnet");
+            if (!pythonnet.StartsWith("3.0"))
+                throw new InvalidProgramException("Expect streamlit 3.0+.");
         }
         #endregion
     }
