c5ad1a52b49ed9437007c7f300b163b7fff91212
Charles Zhang <szinubuntu@gmail.com>
2024-08-06 14:23:20 -0400

    Implement basic layouts, navigation and styles

--- Neo/Parcel.Neo/PopupWindows/PresentationWindow.xaml.cs
+++ Neo/Parcel.Neo/PopupWindows/PresentationWindow.xaml.cs
@@ -19,19 +19,33 @@ namespace Parcel.Neo.PopupWindows
             Owner = parent;
             InitializeComponent();
 
-            if (presentation.Slides.Count == 0)
-                ShowEndSlide();
-            else
-                ShowSlide(CurrentSlide);
+            ShowNextSlide();
         }
+
         public Presentation Presentation { get; }
         #endregion
 
         #region States
-        private int CurrentSlide = 0;
+        private int CurrentSlide = -1;
         #endregion
 
         #region Routines
+        private void ShowPreviousSlide()
+        {
+            if (CurrentSlide > 0)
+                CurrentSlide--;
+            ShowSlide(CurrentSlide);
+        }
+        private void ShowNextSlide()
+        {
+            if (Presentation.Slides.Count > CurrentSlide)
+                CurrentSlide++;
+
+            if (Presentation.Slides.Count <= CurrentSlide)
+                ShowEndSlide();
+            else
+                ShowSlide(CurrentSlide);
+        }
         private void ShowSlide(int index)
         {
             Slide slide = Presentation.Slides[index];
@@ -39,25 +53,25 @@ namespace Parcel.Neo.PopupWindows
             switch (slide.LayoutType)
             {
                 case Slide.SlideLayoutType.TitlePage:
-                    MakePlaceholderSlide(slide);
+                    MakeTitleSlide(Presentation, slide);
                     break;
                 case Slide.SlideLayoutType.SectionPage:
-                    MakePlaceholderSlide(slide);
+                    MakePlaceholderSlide(Presentation, slide);
                     break;
                 case Slide.SlideLayoutType.TableOfContent:
-                    MakePlaceholderSlide(slide);
+                    MakePlaceholderSlide(Presentation, slide);
                     break;
                 case Slide.SlideLayoutType.HeaderWithImage:
-                    MakeHeaderWithImageSlide(slide);
+                    MakeHeaderWithImageSlide(Presentation, slide);
                     break;
                 case Slide.SlideLayoutType.HeaderWithOneBody:
-                    MakePlaceholderSlide(slide);
+                    MakeHeaderWithOneBodySlide(Presentation, slide);
                     break;
                 case Slide.SlideLayoutType.HeadertWithTwoBody:
-                    MakePlaceholderSlide(slide);
+                    MakePlaceholderSlide(Presentation, slide);
                     break;
                 case Slide.SlideLayoutType.HeaderWithThreeBody:
-                    MakePlaceholderSlide(slide);
+                    MakePlaceholderSlide(Presentation, slide);
                     break;
                 default:
                     throw new ArgumentException($"Unkown slide type: {slide.LayoutType}");
@@ -70,27 +84,145 @@ namespace Parcel.Neo.PopupWindows
         #endregion
 
         #region Slides
-        private void MakeHeaderWithImageSlide(Slide slide)
+        private void MakeTitleSlide(Presentation presentation, Slide slide)
         {
             PresentationGrid.Children.Clear();
             PresentationGrid.Background = new SolidColorBrush(Colors.White);
+
+            // Container
+            StackPanel panel = new StackPanel()
+            {
+                Orientation = Orientation.Vertical,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+            };
+            PresentationGrid.Children.Add(panel);
+
+            // Title
+            panel.Children.Add(new Label()
+            {
+                Content = slide.Title,
+                Foreground = new SolidColorBrush(Colors.Black),
+
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                Margin = new System.Windows.Thickness(0),
+                Padding = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize),
+
+                FontSize = slide.SlideStyle.TitleFontSize,
+                FontFamily = new FontFamily(slide.SlideStyle.TitleFont)
+            });
+            // Subtitle
+            if (!string.IsNullOrWhiteSpace(slide.SubTitle))
+                panel.Children.Add(new Label()
+                {
+                    Content = slide.SubTitle,
+                    Foreground = new SolidColorBrush(Colors.Black),
+
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+
+                    FontSize = slide.SlideStyle.SubTitleFontSize,
+                    Margin = new System.Windows.Thickness(0),
+                    Padding = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize),
+
+                    FontFamily = new FontFamily(slide.SlideStyle.HeaderFont)
+                });
+
+            // Footer
+            AddFooter(presentation);
+            // Slide Number
+            AddSlideNumber(presentation, slide);
+        }
+        private void MakeHeaderWithOneBodySlide(Presentation presentation, Slide slide)
+        {
+            PresentationGrid.Children.Clear();
+            PresentationGrid.Background = new SolidColorBrush(Colors.White);
+
+            // Container
+            DockPanel panel = new()
+            {
+                LastChildFill = true,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch,
+                VerticalAlignment = System.Windows.VerticalAlignment.Stretch,
+                Margin = new System.Windows.Thickness(20)
+            };
+            PresentationGrid.Children.Add(panel);
+
+            // Header
+            var headerLabel = new Label()
+            {
+                Content = slide.Header,
+                Foreground = new SolidColorBrush(Colors.Black),
+
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                VerticalAlignment = System.Windows.VerticalAlignment.Top,
+
+                FontSize = slide.SlideStyle.HeaderFontSize,
+                Margin = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize, 0),
+                Padding = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize),
+
+                FontFamily = new FontFamily(slide.SlideStyle.HeaderFont)
+            };
+            DockPanel.SetDock(headerLabel, Dock.Top);
+            panel.Children.Add(headerLabel);
+            
+            // Body
+            var bodyText = new TextBlock()
+            {
+                Text = slide.Body1,
+                Foreground = new SolidColorBrush(Colors.Black),
+
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                TextAlignment = System.Windows.TextAlignment.Left,
+
+                FontSize = slide.SlideStyle.BodyFontSize,
+                Margin = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize, 0, slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize),
+                Padding = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize, 0, slide.SlideStyle.HeaderFontSize, slide.SlideStyle.HeaderFontSize),
+
+                FontFamily = new FontFamily(slide.SlideStyle.BodyFont)
+            };
+            DockPanel.SetDock(bodyText, Dock.Bottom);
+            panel.Children.Add(bodyText);
+
+            // Footer
+            AddFooter(presentation);
+            // Slide Number
+            AddSlideNumber(presentation, slide);
+        }
+        private void MakeHeaderWithImageSlide(Presentation presentation, Slide slide)
+        {
+            PresentationGrid.Children.Clear();
+            PresentationGrid.Background = new SolidColorBrush(Colors.White);
+            // Header
             PresentationGrid.Children.Add(new Label()
             {
                 Content = slide.Header,
-                Foreground = new SolidColorBrush(Colors.Red),
-                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                Foreground = new SolidColorBrush(Colors.Black),
+
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
                 VerticalAlignment = System.Windows.VerticalAlignment.Top,
-                FontSize = 26
+
+                FontSize = slide.SlideStyle.HeaderFontSize,
+                Margin = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize),
+                Padding = new System.Windows.Thickness(slide.SlideStyle.HeaderFontSize),
+
+                FontFamily = new FontFamily(slide.SlideStyle.HeaderFont)
             });
-            PresentationGrid.Children.Add(new System.Windows.Controls.Image()
+            // Body
+            PresentationGrid.Children.Add(new Image()
             {
-                Source = ImageSourceHelper.ConvertToBitmapImage(slide.Image),
-                Width = slide.Image.Width,
+                Source = ImageSourceHelper.ConvertToBitmapImage(slide.Image3),
+                Width = slide.Image3.Width,
+
                 HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
                 VerticalAlignment = System.Windows.VerticalAlignment.Center
             });
+            // Footer
+            AddFooter(presentation);
+            // Slide Number
+            AddSlideNumber(presentation, slide);
         }
-        private void MakePlaceholderSlide(Slide slide)
+        private void MakePlaceholderSlide(Presentation presentation, Slide slide)
         {
             PresentationGrid.Children.Clear();
             PresentationGrid.Background = new SolidColorBrush(Colors.Black);
@@ -106,25 +238,73 @@ namespace Parcel.Neo.PopupWindows
         {
             PresentationGrid.Children.Clear();
             PresentationGrid.Background = new SolidColorBrush(Colors.Black);
-            PresentationGrid.Children.Add(new Label()
+            PresentationGrid.Children.Add(new TextBlock()
             {
-                Content = "Presentation has ended.\nPress ESC to quit.",
+                Text = "Presentation has ended.\nPress ESC to quit.",
                 Foreground = new SolidColorBrush(Colors.White),
                 HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
-                VerticalAlignment = System.Windows.VerticalAlignment.Center
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                TextAlignment = System.Windows.TextAlignment.Center
             });
+        }
+        #endregion
+
+        #region Decorators
+        private void AddFooter(Presentation presentation)
+        {
+            if (presentation.Settings.Footer != null)
+                PresentationGrid.Children.Add(new Label()
+                {
+                    Content = presentation.Settings.Footer,
+                    Foreground = new SolidColorBrush(Colors.DarkKhaki),
+
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                    VerticalAlignment = System.Windows.VerticalAlignment.Bottom,
+
+                    FontSize = presentation.Settings.FooterFontSize,
+                    Margin = new System.Windows.Thickness(presentation.Settings.FooterFontSize),
+                    Padding = new System.Windows.Thickness(presentation.Settings.FooterFontSize),
+
+                    FontFamily = new FontFamily(presentation.Settings.FooterFont)
+                });
+        }
+        private void AddSlideNumber(Presentation presentation, Slide slide)
+        {
+            if (presentation.Settings.ShowSlideNumber)
+                if (presentation.Settings.ShowSlideNumberOnTitlePage || slide.LayoutType != Slide.SlideLayoutType.TitlePage) // Show page number on non-title page by default
+                    PresentationGrid.Children.Add(new Label()
+                    {
+                        Content = $"{presentation.Slides.IndexOf(slide)}",
+                        Foreground = new SolidColorBrush(Colors.DarkKhaki),
+
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
+                        VerticalAlignment = System.Windows.VerticalAlignment.Bottom,
+
+                        FontSize = presentation.Settings.FooterFontSize,
+                        Margin = new System.Windows.Thickness(presentation.Settings.FooterFontSize),
+                        Padding = new System.Windows.Thickness(presentation.Settings.FooterFontSize),
+
+                        FontFamily = new FontFamily(presentation.Settings.FooterFont)
+                    });
         }
         #endregion
 
         #region Events
         private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (Presentation.Slides.Count > CurrentSlide + 1)
-                CurrentSlide++;
+            ShowNextSlide();
         }
         private void BaseWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            DialogResult = true;
+            // Left
+            if (e.Key == System.Windows.Input.Key.Left)
+                ShowPreviousSlide();
+            // Right
+            else if (e.Key == System.Windows.Input.Key.Right)
+                ShowNextSlide();
+            // Exit
+            else if (e.Key == System.Windows.Input.Key.Escape)
+                DialogResult = true;
         }
         #endregion
     }
--- Parcel.NExT/Packages/Zora.SlidePresent/PresentationMaker.cs
+++ Parcel.NExT/Packages/Zora.SlidePresent/PresentationMaker.cs
@@ -2,6 +2,20 @@
 
 namespace Zora.GUI.Feature
 {
+    public sealed class SlideStyle
+    {
+        public int TitleFontSize { get; set; } = 60;
+        public int SubTitleFontSize { get; set; } = 24;
+        public int HeaderFontSize { get; set; } = 44;
+        public int SubHeaderFontSize { get; set; } = 24;
+        public int BodyFontSize { get; set; } = 28;
+
+        public string TitleFont { get; set; } = "Helvetica";
+        public string SubTitleFont { get; set; } = "Helvetica";
+        public string HeaderFont { get; set; } = "Verdana";
+        public string SubHeaderFont { get; set; } = "Verdana";
+        public string BodyFont { get; set; } = "Arial";
+    }
     public sealed class Slide
     {
         public enum SlideLayoutType
@@ -16,17 +30,25 @@ namespace Zora.GUI.Feature
         }
 
         public SlideLayoutType LayoutType { get; set; }
-        public string Title { get; set; }
-        public string Header { get; set; }
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
+        public string? Header { get; set; }
+        public string? SubHeader1 { get; set; }
 
         #region Text Body
-        public string Body1 { get; set; }
-        public string Body2 { get; set; }
-        public string Body3 { get; set; }
+        public string? Body1 { get; set; }
+        public string? Body2 { get; set; }
+        public string? Body3 { get; set; }
         #endregion
 
         #region Image Body
-        public Image Image { get; set; }
+        public Image? Image1 { get; set; }
+        public Image? Image2 { get; set; }
+        public Image? Image3 { get; set; }
+        #endregion
+
+        #region Styling
+        public SlideStyle SlideStyle { get; set; }
         #endregion
     }
     public sealed class Presentation
@@ -37,37 +59,55 @@ namespace Zora.GUI.Feature
     public sealed class PresentationSetting
     {
         public bool ShowSlideNumber { get; set; }
+        public bool ShowSlideNumberOnTitlePage { get; set; } = false;
+
+        public string? Footer { get; set; }
+        public double FooterFontSize { get; set; } = 12;
+        public string FooterFont { get; set; } = "Verdana";
     }
 
     public static class PresentationMaker
     {
         #region Configuration
-        public static PresentationSetting ConfigurePresentation(bool showSlideNumber = true)
+        public static PresentationSetting ConfigurePresentation(bool showSlideNumber = true, string? footer = null)
         {
             return new()
             {
-                ShowSlideNumber = showSlideNumber
+                ShowSlideNumber = showSlideNumber,
+                Footer = footer
             };
         }
         #endregion
 
         #region Slide Making
-        public static Slide OneBodySlide(string header, string body)
+        public static Slide TitleSlide(string title, string? subtitle, SlideStyle? style = null)
+        {
+            return new Slide()
+            {
+                LayoutType = Slide.SlideLayoutType.TitlePage,
+                Title = title,
+                SubTitle = subtitle,
+                SlideStyle = style ?? DefaultSlideStyle
+            };
+        }
+        public static Slide OneBodySlide(string header, string body, SlideStyle? style = null)
         {
             return new Slide()
             {
                 LayoutType = Slide.SlideLayoutType.HeaderWithOneBody,
                 Header = header,
-                Body1 = body
+                Body1 = body,
+                SlideStyle = style ?? DefaultSlideStyle
             };
         }
-        public static Slide OneBodySlide(string header, Image image)
+        public static Slide OneBodySlide(string header, Image image, SlideStyle? style = null)
         {
             return new Slide()
             {
                 LayoutType = Slide.SlideLayoutType.HeaderWithImage,
                 Header = header,
-                Image = image,
+                Image3 = image,
+                SlideStyle = style ?? DefaultSlideStyle
             };
         }
         #endregion
@@ -83,6 +123,11 @@ namespace Zora.GUI.Feature
                 Settings = setting
             };
         }
+        #endregion
+
+        #region Helpers
+        private static SlideStyle DefaultSlideStyle
+            => new();
         #endregion
     }
 }
