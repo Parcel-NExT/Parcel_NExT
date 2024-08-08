a6d74998c1e2cf779fe42055b05073b47e99f82f
Charles Zhang <szinubuntu@gmail.com>
2024-08-08 15:03:02 -0400

    [Package] Zora.Excel: Preliminary implementation

---
+++ Parcel.NExT/Packages/Parcel.Excel/ExcelReader.cs
@@ -0,0 +1,37 @@
+﻿using ExcelDataReader;
+using Parcel.Integration;
+using Parcel.Types;
+using System.Data;
+
+namespace Parcel.FileFormats.Excel
+{
+    /// <summary>
+    /// Reading excel files.
+    /// </summary>
+    /// <remarks>
+    /// We have dedicated reader/writer because implementation may vary.
+    /// </remarks>
+    public static class ExcelReader
+    {
+        public static DataGrid LoadFromExcelWorksheet(string path, string? worksheet = null)
+        {
+            using FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            using IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+
+            string csv = string.Empty;
+            if (string.IsNullOrWhiteSpace(worksheet))
+                csv = result.Tables[0].ToCSV();
+            else
+                csv = result.Tables[worksheet].ToCSV();
+
+            return DataGridIntegration.InitializeDataGridFromCsvText(worksheet ?? Path.GetFileNameWithoutExtension(path), csv);
+        }
+    }
+}
--- Parcel.NExT/Packages/Zora.Expresso/README.md
+++ Parcel.NExT/Packages/Zora.Expresso/README.md
@@ -1,10 +1,17 @@
 # Zora Expresso
 
-Tags: Framework, Data Analytics, Ingest
+Tags: Framework, Data Analytics, Ingest  
+RFC: PENDING
 
-Consumes various tabular source formats and process using SQL.
+A SQL based functional paradigm dataflow processing framework. Consumes various tabular source formats and process using SQL.
 
 We must make sure we write good post/blog, user manual/documentation, YouTube video/animation to introduce this library way otherwise it could easily get ignored and wasted because it's too powerful a paradigm on its own.
+
+## TODO
+
+Management:
+
+- [ ] Consolidate README notes from [old repo](https://github.com/chaojian-zhang/Expresso/blob/master/README.md)
 
 ## Notes
 
