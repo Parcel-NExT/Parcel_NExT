7d6b40490dcf43adb6ebdc6f45fae0c495c57414
Charles Zhang <szinubuntu@gmail.com>
2024-08-09 12:29:10 -0400

    Fix TextNode implementation, provide default empty string value

--- Neo/Parcel.Neo.Base/Framework/ViewModels/Primitives/TextNode.cs
+++ Neo/Parcel.Neo.Base/Framework/ViewModels/Primitives/TextNode.cs
@@ -7,6 +7,7 @@ namespace Parcel.Neo.Base.Framework.ViewModels.Primitives
         public TextNode()
         {
             Title = NodeTypeName = "Text";
+            Value = string.Empty; // Remark-cz: Make sure it's not null, so it's more friendly with serialization
         }
 
         public override OutputConnector MainOutput => ValueOutput as OutputConnector;
